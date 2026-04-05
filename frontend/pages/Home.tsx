722008e794d557db9896070db4dd2740e7e6ee60
SYS <118187096+sysDrawnest@users.noreply.github.com>
2026-04-05 18:29:11 +0530

    Loom Process

--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -24,6 +24,7 @@ import RegisterModal from '../components/RegisterModal';
 import HandwovenHeritage from './sections/HandwovenHeritage';
 import LearningSection from './sections/LearningSection';
 import JournalHint from './sections/JournalHint';
+import LoomProcess from './sections/LoomProcess';
 
 const Home: React.FC = () => {
   const { products, fetchProducts, loading } = useStore();
@@ -112,6 +113,7 @@ const Home: React.FC = () => {
 
         <TrustSignals />
         <MasterWeaverSection />
+        <LoomProcess />
         <JournalHint />
         <LearningSection />
 
